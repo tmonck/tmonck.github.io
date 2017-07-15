96708d36c9c94edde02727437695bde6bb858448
Tom Monck JR <tom.monck@dealertrack.com>
2017-07-15 16:17:22 -0400

    testing something

--- javascripts/myjs.js
+++ javascripts/myjs.js
@@ -1,6 +1,11 @@
-function setAge {
-	var currentDate = new Date();
-	var myBirthDate = new Date(86,3,30)
-	var myAge = (currentDate - myBirthDate)/(1000*60*60*24*365)
-	document.getElementById('age').innerHTML = myAge.toFixed(0);
-}
+var currentDate = new Date();
+var myBirthDate = new Date(86,3,30)
+var oldestBirthDate = new Date(2011,8,29)
+var youngestBirthDate = new Date(2015,4,21)
+var myAge = (currentDate - myBirthDate)/(1000*60*60*24*365)
+var oldest = (currentDate - oldestBirthDate)/(1000*60*60*24*365)
+var youngest = (currentDate - youngestBirthDate)/(1000*60*60*24*365)
+
+document.getElementById("myAge").innerHTML = myAge.toFixed(0);
+document.getElementById("oldestAge").innerHTML = oldest.toFixed(0);
+document.getElementById("youngestAge").innerHTML = youngest.toFixed(0);
