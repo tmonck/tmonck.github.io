413da9c0209e987f60af67765451afdc0858420a
tom <tom@monck.info>
2024-08-22 18:00:17 -0400

    Switch the order of the languages

--- assets/js/myjs.js
+++ assets/js/myjs.js
@@ -1,7 +1,7 @@
 const currentDate = new Date();
 const myBirthDate = new Date(86,3,30);
 const myAge = (currentDate - myBirthDate)/(1000*60*60*24*365);
-const programmingLanguages = ['Golang','C#', 'Javascript', 'Typescript', 'Python', 'Java'];
+const programmingLanguages = ['Golang', 'Java','C#', 'Javascript', 'Typescript', 'Python'];
 programmingLanguages.forEach(function(language, index) {
     if (index === programmingLanguages.length - 1){
         document.getElementById("prgLang").innerHTML += `and ${language}`;
